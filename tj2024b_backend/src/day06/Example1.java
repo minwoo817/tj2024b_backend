eebd0ebf82d02320a248e6a41ead932a80c022f3
minwoo817 <capzhang123@naver.com>
2024-12-16 12:55:28 +0900

    .

--- tj2024b_backend/src/day06/Example1.java
+++ tj2024b_backend/src/day06/Example1.java
@@ -11,25 +11,30 @@ import java.util.Scanner;
 public class Example1 {
 
 	public static void main(String[] args) {
-		String num1 = null; String people1 = null;
-		String num2 = null; String people2 = null;
-		String num3 = null; String people3 = null;
-		while (true) {
+		// [5] 각 사용자에게 입력받은 값들을 저장할 변수 선언
+		String num1 = null; int people1 = 0;
+		String num2 = null; int people2 = 0;
+		String num3 = null; int people3 = 0;
+		while (true) { // [1] while 반복문 
 			System.out.println("====대기번호 발행====");
-			System.out.println("1.등록 2. 출력");
-			Scanner scan = new Scanner(System.in);
-			int menu = scan.nextInt();
-			if(menu == 1) {
+			System.out.println("1.등록 2. 출력"); // [2] 출력함수, print()
+			Scanner scan = new Scanner(System.in); 
+			int menu = scan.nextInt(); // [3] 입력함수, Scanner 객체 필요, nextInt()함수 이용하여 정수 타입을 입력받아 정수 타입 변수에 저장했다.
+			// [4] 조건문, if(흐름조건), 입력받은 값(메뉴선택)에 따른 흐름제어(1이면, 2이면)
+			if(menu == 1) { System.out.println(">>>>대기명단 등록>>>>");
+				// [5] 각 사용자에게 필요한 데이터 입력받기
 				System.out.println("전화번호 : ");
 				String phonenum = scan.next();
 				System.out.println("인원수 : ");
-				String peoplenum = scan.next();
+				int peoplenum = scan.nextInt();
+				// [6] 만약에 비어있는 명단이 있으면 각 입력받은 데이터를 비어있는 곳에 대입하기 , 비어있다. 문자열이면 null, 숫자이면 0 판단, 임의판단
 				if(num1 == null) {num1 = phonenum; people1 = peoplenum;}
 				else if(num2 == null) {num2 = phonenum; people2 = peoplenum;}
 				else if(num3 == null) {num3 = phonenum; people3 = peoplenum;}
 				else {System.out.println("등록실패");}
 			}
-			else if(menu == 2) {
+			else if(menu == 2) { System.out.println(">>>>대기명단 출력>>>>");
+			// [7] 만약에 비어있지 않으면(면단이 있으면) 각각 변수값 출력하기
 				if(num1 != null) {System.out.println("전화번호 : "+num1+"인원수 : "+people1); }
 				if(num2 != null) {System.out.println("전화번호 : "+num2+"인원수 : "+people2); }
 				if(num3 != null) {System.out.println("전화번호 : "+num3+"인원수 : "+people3); }
